84f31410ec6413b73c10dec83bd33326a6a1efa9
RonnieInvestments <ronnieinvestments81@gmail.com>
2026-01-21 10:54:52 +0300

    Learned mapping and added events.

--- reactTrial/src/3GitHubUsers/UsersSection/UserV2.jsx
+++ reactTrial/src/3GitHubUsers/UsersSection/UserV2.jsx
@@ -1,20 +1,31 @@
 function SingeleUserV2 (props) {
 
-    console.log("Props are", props);
+    // console.log("Props are", props);
+    const navigate = () => {
+        console.log ("Navigate to url ", props.url);
+        window.open (props.url, "_blank");
+    }
+
+    // onClick
+    function mouseOverCB (){
+        console.log ("Mouse over ", props.login)
+    }
 
     return (
-        <div>
-            <div>
-                <img src = {props.avatar_url} />
+        <div className= "m-2 rounded-md shadow-md" onMouseEnter = {mouseOverCB}>
+            <div className= "rounded-md">
+                <img className= "rounded-md" src = {props.avatar_url} />
             </div>
-            <ul>
-                <li>
+            <div className = "p-4">
+                <div>
                     Name <b>{props.login}</b>
-                </li>
-                <li>
-                    Url <b>{props.url}</b>
-                </li>
-            </ul>
+                </div>
+                <button
+                    onClick = {navigate}
+                    className = "px-4 py-2 rounded-md text-white bg-blue-600 cursor-pointer active:opacity-50">
+                        GitHub
+                </button>
+            </div>
         </div>
     )
     
--- reactTrial/src/3GitHubUsers/UsersSection/index.jsx
+++ reactTrial/src/3GitHubUsers/UsersSection/index.jsx
@@ -1,40 +1,23 @@
 import SingleUserV2 from "./UserV2";
 import Users from "./../users.json"
+import SingeleUserV2 from "./UserV2";
 
 
 function UsersSection () {
-
-    const user0 = Users[6];
-    const user1 = Users[1];
-    const user2 = Users[10];
-
-    //console.log(user); 
+    function toMap (item, index) {
+        return (
+            <SingeleUserV2 
+            key = {item.id}
+            avatar_url = {item.avatar_url}
+            url = {item.html_url}
+            login = {item.login}
+            />
+        )
+    }
 
     return (
-        <div>
-            <SingleUserV2 
-            /* country = {"Kenya"}
-            address = {"P.O BOX 1328, Kisii"}
-            pet = {{name: "Blackie", type: "dog", breed: "German Shepherd"}}
-            colors = {["black", "red", "blue", "green", "yellow"]} */
-
-            avatar_url = {user0.avatar_url}
-            login = {user0.login}
-            url = {user0.html_url}
-            />
-
-            <SingleUserV2 
-            avatar_url = {user1.avatar_url}
-            login = {user1.login}
-            url = {user1.html_url}
-            />
-
-            <SingleUserV2 
-            avatar_url = {user2.avatar_url}
-            login = {user2.login}
-            url = {user2.html_url}
-            />
-        </div>
+        <div className="flex flex-wrap">{Users.map(toMap)}
+        </div> 
         
     )
 }

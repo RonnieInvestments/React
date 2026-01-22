d63b787c992bd65cbf0bd3d3d0168c5fbd83e52c
RonnieInvestments <ronnieinvestments81@gmail.com>
2026-01-22 15:58:39 +0300

    Added state management code.

--- reactTrial/src/3GitHubUsers/index.jsx
+++ reactTrial/src/3GitHubUsers/index.jsx
@@ -1,10 +1,12 @@
 import SearchSection from "./SearchSection";
 import UsersSection from "./UsersSection";
+import StateNumbers from "./LearningSection/StateNumbers";
 
 function App () {
     return (
         <div>
         <h1>GitHub Users</h1>
+        <StateNumbers />
         <SearchSection />
         <UsersSection/>
         </div>
